94b06dd8b7703de2ad25b066d3238ff5b9e36721
HR <hrcode275@gmail.com>
2025-03-08 14:02:48 -0600

    Corrección de rutas

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,10 @@ import { setRoutes } from './routes';
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+    res.send("¡La aplicación está funcionando!");
+});
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,3 +18,5 @@ setRoutes(app);
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+
